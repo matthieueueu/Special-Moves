d738def6c6e6b7a3859b80a2ad2a2e61b4799fec
Matt MIC <matthieumichelet99@gmail.com>
2024-01-11 14:44:55 +0200

    new changes 11.01.24 14:44

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const print = console.log;
 
 
+
 let specialMove = [
-    "belt",
-    "chancla",
-    "wooden spoon",
+    "run over by a car",
+    "waterboarding",
+    "burning",
     "rusted shovel",
     "H-Bomb",
 ]
+
+let energyCosts = [
+    10,
+    10,
+    5,
+    5,
+    50,
+]
+
+function onClickCar(){
+    document.getElementById("car1").innerHTML += `<img src="https://i.makeagif.com/media/4-17-2014/_lM7eX.gif" alt="">`
+}
+
+
+function onClickWater(){
+
+}
+
+
+function onClickBurn(){
+    
+}
+
+
+function onClickShovel(){
+    
+}
+
+
+function onClickBomb(){
+    
+}
