6ebe824ee2f125864b0c9f629ab9b7d6b358c972
Matt MIC <matthieumichelet99@gmail.com>
2024-01-11 18:04:31 +0200

    new changes 11.01.24 18:04

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const print = console.log;
 
-
+let energy = 100
 
 let specialMove = [
     "run over by a car",
@@ -18,9 +18,17 @@ let energyCosts = [
     50,
 ]
 
+print(energyCosts[0])
+
+
+
 function onClickCar(){
     document.getElementById("car1").innerHTML += `<img src="https://i.makeagif.com/media/4-17-2014/_lM7eX.gif" alt="">`
+
+    energy = energy-energyCosts[0]
+    print(energy)
 }
+
 
 
 function onClickWater(){
@@ -41,3 +49,5 @@ function onClickShovel(){
 function onClickBomb(){
     
 }
+
+document.getElementById("energy").innerHTML += `Energy level = ${energy}`
