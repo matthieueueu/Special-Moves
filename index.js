8d90a01426740f4612cc2bfbfce58b866e124ac9
Matt MIC <matthieumichelet99@gmail.com>
2024-01-25 13:41:42 +0200

    new changes 25.01.24  13:41

--- index.js
+++ index.js
@@ -13,17 +13,25 @@ let specialMove = [
 let energyCosts = [
     10,
     10,
-    5,
+    20,
     5,
     50,
 ]
 
 print(energyCosts[0])
 
-
+function checkEnergy(){
+    if (energy < 1){
+        document.getElementById("body").innerHTML = `<h2>GAME OVER</h2>`
+        document.getElementById("image").innerHTML = ``
+        energy = 0;
+        document.getElementById("energy").innerHTML = `Energy level = ${energy}`;
+        document.getElementById("addEnergy").disabled = true;
+    }
+}
 
 function onClickCar(){
-    document.getElementById("car1").innerHTML = `<img src="https://i.makeagif.com/media/4-17-2014/_lM7eX.gif" alt="">`
+    document.getElementById("image").innerHTML = `<img src="https://i.makeagif.com/media/4-17-2014/_lM7eX.gif" alt="">`
     // change energy on HTML page
     // 1. select the #energy div
     // 2. change the innerHTML
@@ -31,51 +39,74 @@ function onClickCar(){
     energy = energy-energyCosts[0]
     document.getElementById("energy").innerHTML = `Energy level = ${energy}`
     print(energy)
+    checkEnergy()
 }
 
 
 
 function onClickWater(){
-    document.getElementById("water1").innerHTML = `<img src="https://media.tenor.com/9mN_RX9_FPsAAAAM/kamen-rider-nigo-waterboarding.gif" alt="">`
+    document.getElementById("image").innerHTML = `<img src="https://media.tenor.com/9mN_RX9_FPsAAAAM/kamen-rider-nigo-waterboarding.gif" alt="">`
     // change energy on HTML page
     // 1. select the #energy div
     // 2. change the innerHTML
     
     energy = energy-energyCosts[1]
     document.getElementById("energy").innerHTML = `Energy level = ${energy}`
+    checkEnergy()
+
 }
 
 
 function onClickBurn(){
-    document.getElementById("burn1").innerHTML = `<img src="https://media1.tenor.com/m/qQv_BSE2MOoAAAAC/burning-man-burning-bike.gif" alt="">`
+    document.getElementById("image").innerHTML = `<img src="https://media1.tenor.com/m/qQv_BSE2MOoAAAAC/burning-man-burning-bike.gif" alt="">`
     // change energy on HTML page
     // 1. select the #energy div
     // 2. change the innerHTML
     
     energy = energy-energyCosts[2]
     document.getElementById("energy").innerHTML = `Energy level = ${energy}`
+    checkEnergy()
+
 }
 
 
 function onClickShovel(){
-    document.getElementById("shovel1").innerHTML = `<img src="https://media.tenor.com/zqDK-5_B1c8AAAAC/fight-shovel.giff" alt="">`
+    document.getElementById("image").innerHTML = `<img src="https://media.tenor.com/zqDK-5_B1c8AAAAC/fight-shovel.giff" alt="">`
     // change energy on HTML page
     // 1. select the #energy div
     // 2. change the innerHTML
     
     energy = energy-energyCosts[3]
     document.getElementById("energy").innerHTML = `Energy level = ${energy}`
+    checkEnergy()
+
+
 }
 
 
 function onClickBomb(){
-    document.getElementById("bomb1").innerHTML = `<img src="https://33.media.tumblr.com/cf7f1b12d52bb5d9b3cafe88282560b0/tumblr_ndrvh35bP11ttwc1to1_400.gif" alt="">`
+    document.getElementById("image").innerHTML = `<img src="https://33.media.tumblr.com/cf7f1b12d52bb5d9b3cafe88282560b0/tumblr_ndrvh35bP11ttwc1to1_400.gif" alt="">`
     // change energy on HTML page
     // 1. select the #energy div
     // 2. change the innerHTML
     
     energy = energy-energyCosts[4]
     document.getElementById("energy").innerHTML = `Energy level = ${energy}`
+    checkEnergy()
+}
+
+function onClickGetMoreEnergy(){
+    if (energy <100){
+        energy += 1;
+        document.getElementById("energy").innerHTML = `Energy level = ${energy}`;
+    } else if (energy>100){
+        document.getElementById("addEnergy").disabled = true;
+    }
 }
 
 document.getElementById("energy").innerHTML += `Energy level = ${energy}`
+
+
+
+
+
